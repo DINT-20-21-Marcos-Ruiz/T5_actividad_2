d3edbbb30f5091cf44325bc8ab8b8e1ccf3c7d31
Marcos <marcosruiz.16@iesdoctorbalmis.com>
2020-12-27 18:41:05 +0100

    Agregar archivos de proyecto.

---
+++ T5_actividad_2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T5_actividad_2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Plato> listaPlatos = Plato.GetSamples("/Resources");
+        List<String> tipoComida = new List<String> { "China", "Americana", "Mexicana" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListadoPlatos.DataContext = listaPlatos;
+            tipo_ComboBox.ItemsSource = tipoComida;
+        }
+    }
+}
